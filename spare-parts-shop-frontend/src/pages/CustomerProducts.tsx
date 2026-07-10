6f04319246390b2b8cd19780bb2957ca495fad06
vip-211 <vipinchaurasiya1052@gmail.com>
2026-07-10 10:03:41 +0530

    fix: remove size prop from MUI Search icon

--- spare-parts-shop-frontend/src/pages/CustomerProducts.tsx
+++ spare-parts-shop-frontend/src/pages/CustomerProducts.tsx
@@ -37,7 +37,7 @@ const CustomerProducts = () => {
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
-                <Search size={20} />
+                <Search fontSize="small" />
               </InputAdornment>
             ),
           }}
